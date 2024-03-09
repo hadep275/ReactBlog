4901a3abdd64cc5636a3952524d976b19dba6bbc
hadep275 <hadep275@gmail.com>
2024-03-08 23:31:46 -0700

    firebase init

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -1,13 +1,21 @@
 import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  // firebase
+  apiKey: "AIzaSyDcefBXEJDiNdQnJkiWhuCBEw7qarbKoWk",
+  authDomain: "scale-finance.firebaseapp.com",
+  projectId: "scale-finance",
+  storageBucket: "scale-finance.appspot.com",
+  messagingSenderId: "36284345710",
+  appId: "1:36284345710:web:2d6f3e9991f04ebd9fe57f",
+  measurementId: "G-D8YQ4MNN6L"
 };
 
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 const auth = getAuth(app);
 const db = getFirestore(app);
 const storage = getStorage(app);
